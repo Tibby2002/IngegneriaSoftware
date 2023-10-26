61e82308eea93d4867d24acd35dfa060e46a9791
Tibby2002 <leotiba02@gmail.com>
2023-10-26 09:53:25 +0200

    add selected chain

--- app/src/main/java/com/example/cryptotracker/AddAssets.java
+++ app/src/main/java/com/example/cryptotracker/AddAssets.java
@@ -3,6 +3,8 @@ package com.example.cryptotracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddAssets extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class AddAssets extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_assets);
+
+        Spinner spinner = (Spinner) findViewById(R.id.choices_wallet);
+// Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.possible_wallets,
+                android.R.layout.simple_spinner_item
+        );
+// Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner.
+        spinner.setAdapter(adapter);
     }
 }
