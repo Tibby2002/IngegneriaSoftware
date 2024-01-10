c8b34a863c92743bd47a0665d892880fcc51611a
Zardin Nicoló <zardin.nicolo@gmail.com>
2024-01-10 19:27:15 +0100

    refactor sharedPreferencesManager, update xml profile, add user data and update on shared preferences on update profile

--- app/src/main/java/com/example/cryptotracker/ProfileFragment.java
+++ app/src/main/java/com/example/cryptotracker/ProfileFragment.java
@@ -7,22 +7,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import com.example.cryptotracker.Supports.SharedPreferencesManager;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.io.File;
 
 public class ProfileFragment extends Fragment {
+    private EditText nameText;
+    private EditText surnameText;
+    private EditText emailText;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        String name = SharedPreferencesManager.getString(getContext(), "name", "Nome");
+        String surname = SharedPreferencesManager.getString(getContext(), "surname", "Cognome");
+        String email = SharedPreferencesManager.getString(getContext(), "email", "Email");
+
+        nameText = rootView.findViewById(R.id.nameText);
+        surnameText = rootView.findViewById(R.id.surnameText);
+        emailText = rootView.findViewById(R.id.emailText);
+
+        loadUserData(name, surname, email);
 
         Button changePasswordButton = rootView.findViewById(R.id.buttonChangePassword);
         Button saveButton = rootView.findViewById(R.id.buttonSave);
@@ -33,7 +48,29 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAndUpdateUserData(name, surname);
+                Navigation.findNavController(v).navigate(R.id.nav_home);
+            }
+        });
+
         return rootView;
     }
 
+    private void loadUserData(String name, String surname, String email) {
+        nameText.setText(name);
+        surnameText.setText(surname);
+        emailText.setText(email);
+    }
+
+    private void checkAndUpdateUserData(String name, String surname) {
+        if (!name.equals(nameText.getText().toString())) {
+            SharedPreferencesManager.saveString(getContext(), "name", nameText.getText().toString());
+        }
+        if (!surname.equals(surnameText.getText().toString())) {
+            SharedPreferencesManager.saveString(getContext(), "surname", surnameText.getText().toString());
+        }
+    }
 }
--- app/src/main/java/com/example/cryptotracker/Supports/SharedPreferencesManager.java
+++ app/src/main/java/com/example/cryptotracker/Supports/SharedPreferencesManager.java
@@ -6,43 +6,36 @@ public class SharedPreferencesManager {
 
     private static final String PREFERENCES_NAME = "CryptoTrackerPreferences";
 
-    // Metodo per ottenere un'istanza delle SharedPreferences
     private static SharedPreferences getSharedPreferences(Context context) {
         return context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
     }
 
-    // Metodo per salvare una stringa nelle SharedPreferences
     public static void saveString(Context context, String key, String value) {
         SharedPreferences.Editor editor = getSharedPreferences(context).edit();
         editor.putString(key, value);
         editor.apply();
     }
 
-    // Metodo per ottenere una stringa dalle SharedPreferences
     public static String getString(Context context, String key, String defaultValue) {
         return getSharedPreferences(context).getString(key, defaultValue);
     }
 
-    // Metodo per salvare un boolean nelle SharedPreferences
     public static void saveBoolean(Context context, String key, boolean value) {
         SharedPreferences.Editor editor = getSharedPreferences(context).edit();
         editor.putBoolean(key, value);
         editor.apply();
     }
 
-    // Metodo per ottenere un boolean dalle SharedPreferences
     public static boolean getBoolean(Context context, String key, boolean defaultValue) {
         return getSharedPreferences(context).getBoolean(key, defaultValue);
     }
 
-    // Metodo per rimuovere un dato dalle SharedPreferences
     public static void removeKey(Context context, String key) {
         SharedPreferences.Editor editor = getSharedPreferences(context).edit();
         editor.remove(key);
         editor.apply();
     }
 
-    // Metodo per cancellare tutte le SharedPreferences
     public static void clearPreferences(Context context) {
         SharedPreferences.Editor editor = getSharedPreferences(context).edit();
         editor.clear();
